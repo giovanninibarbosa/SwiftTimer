f2dc2ffcd98ff2298870f5a927414e2a479495d7
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2016-06-23 19:32:20 -0300

    First commit.

--- SwiftTimer/ViewController.swift
+++ SwiftTimer/ViewController.swift
@@ -9,6 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timerCount = 0
+    var timerRunning = false
+    var timer = NSTimer()
+    
+
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    func Counting(){
+        timerCount += 1
+        timerLabel.text = "\(timerCount)"
+    }
+    
+    @IBAction func startButton(sender: UIButton) {
+        if timerRunning == false {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.Counting) , userInfo: nil, repeats: true)
+            timerRunning = true
+        }
+    }
+    
+    @IBAction func stopButton(sender: UIButton) {
+        if timerRunning  == true{
+            timer.invalidate()
+            timerRunning = false
+        }
+    }
+    
+    @IBAction func restartButton(sender: UIButton) {
+        
+        timerCount = 0
+        timerLabel.text = "0"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
